f047fe568624b5b7a246ae2e02944287859a31b4
damilurg <rootdamilurg@gmail.com>
2019-08-16 11:08:27 +0600

    removed old functions

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -24,38 +24,12 @@ $(document).ready(function() {
         }]
     });
 });
-// Elements of navigation
-const anchors = document.querySelectorAll('nav a[href*="#"]');
-let menuButton = document.querySelector("#menu-button");
-let backClick = document.querySelector(".click");
-let drawer = document.querySelector("#drawer");
 
-let boll = false;
 
 // Elements of work page
 let categories = document.querySelectorAll(".work-categories button");
 let works = document.querySelectorAll(".work");
 
-
-
-backClick.addEventListener("click", function() {
-    drawer.className = "";
-    backClick.style.display = "none";
-
-});
-
-menuButton.addEventListener("click", function() {
-    boll = !boll;
-
-    if (boll) {
-
-        drawer.className = "active";
-        backClick.style.display = "block";
-    } else {
-        drawer.className = "";
-
-    }
-});
 
 categories.forEach(category => {
     category.addEventListener("click", function() {
@@ -76,37 +50,7 @@ categories.forEach(category => {
 });
 
 
-for (let anchor of anchors) {
-    anchor.addEventListener("click", function(e) {
-        e.preventDefault();
 
-        drawer.className = "";
-        backClick.style.display = "none";
-
-
-        const blockID = anchor.getAttribute("href");
-
-        if (anchor.getAttribute("href") === "#") {
-            document.querySelector("body").scrollIntoView({
-                behavior: "smooth",
-                block: "start"
-            });
-        } else {
-            document.querySelector("" + blockID).scrollIntoView({
-                behavior: "smooth",
-                block: "start"
-            });
-        }
-
-        for (let link of anchors) {
-            if (link.getAttribute("href") === blockID) {
-                link.className = "active";
-            } else {
-                link.className = "";
-            }
-        }
-    });
-}
 
 //videoplayer
 function videoPlay() {
